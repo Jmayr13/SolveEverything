b152d6ea76cf25351a783fe08418ae8a9d230026
Jmayr13 <jmayr13@gmail.com>
2016-02-14 22:16:02 -0500

    Program works, some bugs need to be fixed, like when there is no solution to the equation there needs to be a text that alerts the user that there is no solution. Also need to work on the interface and design of the site, but the program works.

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 /**
- * Created by Josh on 2/7/2016.
+ * Created by Josh on 2/14/2016.
  */
-var algebra = require('algebra.js');
 var Fraction = algebra.Fraction;
 var Expression = algebra.Expression;
 var Equation = algebra.Equation;
-//The main function that will be started after the user inputs an equation
-function solve() {
-    findXIntercept();
 
-}
-function findXIntercept() {
-    var equationToSolve = algebra.parse(getElementById("equationInput"));
-
-    var cubic = new Equation(equationToSolve, 0);
-
-
-   // console.log(cubic.toString());
-
-    var answers = cubic.solveFor("x");
-
-   // console.log("x = " + answers.toString());
+function solve(){
+    findingXIntercept();
+    findingYIntercept();
 }
 
 
-//This function takes the users input and prepares it to be solved
-function findVars() {
-    var input = getElementById("equationInput");
-    if (input != null) {
 
-    }
+
+function findingXIntercept() {
+    //var linearEquationInput = prompt("Enter an equation: ");
+    var linearEquationInput = algebra.parse(document.getElementById("linearEquationInput").value);
+    //var linearEquationInput = algebra.parse("x^3 + 9x^2 + 26x + 24");
+    ///document.getElementById("demo").innerHTML = linearEquationInput.toString();
+    //console.log(typeof String(linearEquationInput));
+    //var linearEquationString = String(linearEquationInput);
+    //document.getElementById("demo").innerHTML = "linearEquationString";
+    var linearEquationSolveX = new Equation(linearEquationInput, 0);
+    //document.getElementById("demo1").innerHTML = linearEquationSolveX;
+    var linearAnswer = linearEquationSolveX.solveFor("x");
+    var linearAnswers = linearAnswer.toString();
+    katex.render("x = " + linearAnswers, mykatex1, {displayMode: true});
+    ///document.getElementById("equationOutput").innerHTML = linearAnswer.toString();
+    //document.getElementById("demo2").innerHTML = linearAnswers;
+    //document.getElementById("equationOutput").innerHTML = linearAnswers;
+    //console.log(linearAnswers);
+}
+
+function findingYIntercept() {
+    var linearEquationInput = document.getElementById("linearEquationInput").value;
+    //var linearEquationInput = "x+5";
+    var yValue = algebra.parse("y");
+    var linearEquationInputReplace = linearEquationInput.replace(/x/g, "0");
+    // console.log(linearEquationInputReplace)
+    var linearEquationInputEquation = algebra.parse(linearEquationInputReplace);
+    var linearEquationSolveY = new Equation(yValue, linearEquationInputEquation);
+    var yInterceptAnswers = linearEquationSolveY.solveFor("y");
+    var yInterceptAnswers = yInterceptAnswers.toString();
+    katex.render("y = " + yInterceptAnswers, mykatex2, {displayMode: true});
+    //console.log(yInterceptAnswers.toString());
 }
