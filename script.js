232f55be9ca9e7ec453c50ca87e8506a52e00e69
ryanatkins88 <ratkins1584@gmail.com>
2016-02-24 14:53:13 -0500

    added elseif condition

--- script.js
+++ script.js
@@ -18,12 +18,21 @@ function findingXIntercept() {
     var linearEquationSolveX = new Equation(linearEquationInput, 0);
     var linearAnswer = linearEquationSolveX.solveFor("x");
     //Returns no solution if the equation does not cross the x-axis, prints the answer if there is one
-    if (linearAnswer == "") {
-        document.getElementById("mykatex1").innerHTML = "No solution";
-    } else {
+    if (linearAnswer == "0") {
+        document.getElementById("mykatex1").innerHTML = "0";
+        var linearAnswers = linearAnswer.toString();
+        katex.render("x = " + linearAnswers, mykatex1, {displayMode: true});
+    } else if (linearAnswer == "") {
+        document.getElementById("mykatex1").innerHTML = "no solution";
+    } else if(linearAnswer == "" && linearAnswer != "") {
         var linearAnswers = linearAnswer.toString();
         katex.render("x = " + linearAnswers, mykatex1, {displayMode: true});
     }
+    else {
+        var linearAnswers = linearAnswer.toString();
+        katex.render("x = " + linearAnswers, mykatex1, {displayMode: true});
+    }
+    //console.log(linearAnswer.typeOf)
 }
 
 function findingYIntercept() {
@@ -33,7 +42,11 @@ function findingYIntercept() {
     var linearEquationInputEquation = algebra.parse(linearEquationInputReplace);
     var linearEquationSolveY = new Equation(yValue, linearEquationInputEquation);
     var yInterceptAnswer = linearEquationSolveY.solveFor("y");
-    if (yInterceptAnswer == "") {
+    if (yInterceptAnswer == "0") {
+        document.getElementById("mykatex2").innerHTML = "0";
+        var yInterceptAnswers = yInterceptAnswer.toString();
+        katex.render("y = " + yInterceptAnswers, mykatex2, {displayMode: true});
+    } else if (yInterceptAnswer == "") {
         document.getElementById("mykatex2").innerHTML = "No solution";
     } else {
         var yInterceptAnswers = yInterceptAnswer.toString();
